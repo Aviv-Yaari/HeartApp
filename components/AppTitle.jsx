32a01cb0eebd4b681c62bb35bcc5c49cabd28d24
Aviv-Yaari <avivyar@gmail.com>
2021-10-30 20:06:40 +0300

    added shadow to headings

--- components/AppTitle.jsx
+++ components/AppTitle.jsx
@@ -12,5 +12,7 @@ const style = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     color: "#F23864",
+    textShadowColor: "rgba(125, 0, 0, 0.35)",
+    textShadowRadius: 2,
   },
 });
